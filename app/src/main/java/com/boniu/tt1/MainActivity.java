6fe69638cb89a65a4360ebce3f3fea127ed7506a
Boniu <>
2020-06-03 11:54:42 +0800

    第一次提交

---
+++ app/src/main/java/com/boniu/tt1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.boniu.tt1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
